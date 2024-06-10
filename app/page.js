ef2767a25ba5855ae3e59f95a47f4bac4a52319d
Shashwat Jain <kcapshashwat@gmail.com>
2024-06-10 21:56:13 +0530

    add the map feature and made it responsive

--- app/page.js
+++ app/page.js
@@ -95,7 +95,7 @@ export default function Home() {
 
       {/* <Map /> */}
 
-      {/* <MapComponent /> */}
+      <MapComponent />
 
 
       <Contact />
--- components/contact.js
+++ components/contact.js
@@ -118,14 +118,14 @@ export default function ContactMe() {
                         placeholder="Your Name"
                         name="from_name"
                         required
-                        className="mb-2 h-10 max-w-xl rounded-lg border-2 bg-white p-5 text-sm hover:border-cyan-500 dark:border-transparent dark:bg-gray-900 dark:hover:border-gray-700"
+                        className="mb-2 h-10 max-w-xl rounded-lg border-2 bg-white p-5 text-sm hover:border-cyan-500 dark:border-transparent dark:bg-white dark:hover:border-gray-700"
                     />
                     <input
                         type="email"
                         placeholder="Your Email"
                         name="from_email"
                         required
-                        className="mb-2 h-10 max-w-xl rounded-lg border-2 bg-white p-5 text-sm hover:border-cyan-500 dark:border-transparent dark:bg-gray-900 dark:hover:border-gray-700"
+                        className="mb-2 h-10 max-w-xl rounded-lg border-2 bg-white p-5 text-sm hover:border-cyan-500 dark:border-transparent dark:bg-white dark:hover:border-gray-700"
                     />
                     <textarea
                         rows={5}
@@ -133,7 +133,7 @@ export default function ContactMe() {
                         name="message"
                         minLength={10}
                         required
-                        className="max-w-xl rounded-lg border-2 bg-white p-5 text-sm hover:border-cyan-500 dark:border-transparent dark:bg-gray-900 dark:hover:border-gray-700"
+                        className="max-w-xl rounded-lg border-2 bg-white p-5 text-sm hover:border-cyan-500 dark:border-transparent dark:bg-white dark:hover:border-gray-700"
                     />
                     <button
                         type="submit"
@@ -141,8 +141,8 @@ export default function ContactMe() {
                         className={`btn group relative mt-5 inline-flex h-12 w-40 items-center justify-start overflow-hidden rounded-lg border-2 bg-white text-sm transition-all dark:border-0 dark:bg-gray-900`}
                     >
                         <span
-                            className={`-z-1 absolute left-0 top-0 h-0 w-0 rounded ${status === 'Sent!' ? 'bg-green-500 text-black' : 'bg-cyan-600'
-                                } transition-all duration-500 ease-out group-hover:h-full group-hover:w-full ${status === 'Sent!' ? 'text-white dark:bg-green-500' : 'dark:bg-cyan-700'
+                            className={`-z-1 absolute left-0 top-0 h-0 w-0 rounded ${status === 'Sent!' ? 'bg-green-500 text-white' : 'bg-cyan-600'
+                                } transition-all duration-500 ease-out group-hover:h-full group-hover:w-full ${status === 'Sent!' ? 'text-black dark:bg-green-500' : 'dark:bg-cyan-700'
                                 }`}
                         ></span>{' '}
                         <span className="z-10 w-full text-gray-400 transition-colors duration-300 ease-in-out group-hover:text-white">
@@ -153,7 +153,7 @@ export default function ContactMe() {
 
 
             </div>
-            <footer className="flex flex-row justify-center items-center bg-gray-800 text-white p-4 text-sm space-x-2">
+            <footer className="flex flex-row justify-center items-center bg-gray-800 text-white p-4 text-sm space-x-1">
                 <p className='text-xs text-balance text-center'>©2024 |</p>
                 <p className='text-xs text-balance text-center'>Designed By Shashwat Jain |</p>
                 <p className='text-xs text-balance text-center'>All rights reserved.</p>
