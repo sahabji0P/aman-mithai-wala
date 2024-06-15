05f6af26b4e57a75485883aec9d68d24ee4403a5
Shashwat Jain <kcapshashwat@gmail.com>
2024-06-15 13:04:52 +0530

    edited the map and removed the extra files and imports

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './MapComponent.module.css';
-import map from '../public/ITSOK.png';
 import map2 from '../public/map2.png';
 import Image from 'next/image';
 
@@ -61,7 +60,7 @@ const MapComponent = () => {
                                 <iframe
                                     style={{ height: '100%', width: '100%', border: 0 }}
                                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3500.153898552776!2d77.32832599999999!3d28.685042599999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfbac08c80ea1%3A0x8fccc5f28700d00d!2sAman%20Mithaiwala%20pvt.%20Ltd!5e0!3m2!1sen!2sin!4v1718382036157!5m2!1sen!2sin"
-                                    allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    allowFullScreen="" loading="lazy"></iframe>
                             </div>
 
 
@@ -84,7 +83,7 @@ const MapComponent = () => {
                                 <iframe
                                     style={{ height: '100%', width: '100%', border: 0 }}
                                     src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3725.22742583886!2d75.58726387525539!3d20.983518780654453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjDCsDU5JzAwLjciTiA3NcKwMzUnMjMuNCJF!5e0!3m2!1sen!2sin!4v1718365313804!5m2!1sen!2sin"
-                                    allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
                             </div>
                         </div>
